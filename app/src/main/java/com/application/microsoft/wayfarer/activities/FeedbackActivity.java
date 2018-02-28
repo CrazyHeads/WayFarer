68bc61b73cdc162c012343e141e98c91005f287f
Sai Harika Prathi <harikaprathi@gmail.com>
2018-02-28 17:19:34 +0530

    Updated FeedbackActivity.java

--- app/src/main/java/com/application/microsoft/wayfarer/activities/FeedbackActivity.java
+++ app/src/main/java/com/application/microsoft/wayfarer/activities/FeedbackActivity.java
@@ -1,1 +1,45 @@
+package com.application.microsoft.wayfarer.activities;
+
+/**
+ * Created by satyavati on 28-02-2018.
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.application.microsoft.wayfarer.R;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.application.microsoft.wayfarer.R;
+
+
+public class FeedbackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+    }
+
+    public void onSubmit(View v) {
+        Intent myIntent = new Intent(FeedbackActivity.this, MenuActivity.class);
+        startActivity(myIntent);
+    }
+
+}
+
 
