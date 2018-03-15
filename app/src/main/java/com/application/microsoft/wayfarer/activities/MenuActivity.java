239878368482dc9cdcb585260ca943ccca71891f
PoojaVennamaneni <pooja.vennamaneni@gmail.com>
2018-03-15 12:23:56 -0700

    Added comments to MenuActivity

--- app/src/main/java/com/application/microsoft/wayfarer/activities/MenuActivity.java
+++ app/src/main/java/com/application/microsoft/wayfarer/activities/MenuActivity.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 import com.application.microsoft.wayfarer.R;
-
+/* Once the user logs in,he can Plan his trip,Start his trip or give feddback in the Menu Activity*/
 
 public class MenuActivity extends AppCompatActivity {
     SharedPreferences sharedPreferences;
@@ -32,15 +32,15 @@ public class MenuActivity extends AppCompatActivity {
         }
         System.out.println(name);
 
-
+/* If the user clicks on plan card,he will be directed  to MainActivity where he can plan his trip*/
        mycard = (CardView) findViewById(R.id.plan);
         i = new Intent(this,MainActivity.class);
         mycard.setOnClickListener(v -> startActivity(i));
-
+/* If the user clicks on Myplan card,he will be directed  to PlanActivity where he can start his trip*/
         mycard1 = (CardView) findViewById(R.id.myPlan);
         i1 = new Intent(this,MyPlanActivity.class);
         mycard1.setOnClickListener(v -> startActivity(i1));
-
+/* If the user clicks on Feedback card,he will be directed  to FeedbackActivity where he can give feedback*/
 
         mycard2 = (CardView) findViewById(R.id.feedback);
         i2 = new Intent(this,FeedbackActivity.class);
